e1635ecbc4215b4ac79dc40bf81c210c727abb81
Steve Bate <stephen.bate@mdams.com>
2020-04-12 10:31:15 +0100

    improved ViewMessage filter to be able to dump message body from older versions of MiniBus and even NServiceBus too

--- src/MiniBus/Filters/ViewMessage.cs
+++ src/MiniBus/Filters/ViewMessage.cs
@@ -15,11 +15,27 @@ namespace MiniBus.Filters
         public void Execute(MessageContext ctx)
         {
             var payload = (string)ctx.Message.Body;
-            var stripped = payload.Substring(0, payload.Length - 1).Replace("{\"Payload\":", "");
+
+            var stripped = payload;
+
+            if (stripped.StartsWith("\""))
+            {
+                stripped = stripped.Substring(0, payload.Length - 1);
+            }
+
+            if (stripped.Contains("\"Payload\":"))
+            {
+                stripped = stripped.Replace("{\"Payload\":", "");
+                stripped = stripped.Substring(0, stripped.Length - 1);
+            }
+
             if (stripped.Contains("xml"))
             {
-                stripped = stripped.Substring(1, stripped.Length - 2);
+                stripped = stripped.StartsWith("\"")
+                    ? stripped.Substring(1, stripped.Length - 2)
+                    : stripped.Substring(0, stripped.Length - 1);
             }
+
             ctx.OnStep($"Message: {ctx.Message.Label} - peeked from queue: {ctx.ReadQueue.FormatName}");
 
             _output(stripped);
